30fa76e5501fe46d48093202f5cb4db54cb2925b
David-Lisboa <dlrlisboa@gmail.com>
2021-06-20 02:14:20 -0400

    app_bar.xml inplementado

--- app/src/main/java/com/davidproject/whatsapp/MainActivity.kt
+++ app/src/main/java/com/davidproject/whatsapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.davidproject.whatsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(main_toolbar as Toolbar?)
+        supportActionBar?.title = "Whatsapp"
     }
 }
