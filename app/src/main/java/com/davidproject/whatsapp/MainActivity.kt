002f8df978e65f45150c8a9c7f80f878b62b4186
David-Lisboa <dlrlisboa@gmail.com>
2021-06-22 11:01:24 -0400

    erro

--- app/src/main/java/com/davidproject/whatsapp/MainActivity.kt
+++ app/src/main/java/com/davidproject/whatsapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.davidproject.whatsapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -7,11 +8,12 @@ import com.davidproject.whatsapp.Adapter.TabSwitcher
 import com.davidproject.whatsapp.Fragment.ContactFragment
 import com.davidproject.whatsapp.Fragment.GroupChatFragment
 import com.davidproject.whatsapp.Fragment.ChatFragment
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-   // private lateinit var tabSwitcher: TabSwitcher
+   var currentUser : FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -20,14 +22,25 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(main_toolbar as Toolbar?)
         supportActionBar?.title = "Whatsapp"
-//
-//        tabSwitcher = TabSwitcher(supportFragmentManager)
-//        main_view_pager.adapter = tabSwitcher
-//        tab_switcher.setupWithViewPager(main_view_pager)
+
 
         main_view_pager.adapter = TabSwitcher(supportFragmentManager)
         tab_switcher.setupWithViewPager(main_view_pager)
 
     }
+//
+//    override fun onStart(){
+//        super.onStart()
+//
+//        if(currentUser == null){
+////            sendToLoginActivity()
+//        }
+//
+//    }
+//
+//    private fun sendToLoginActivity() {
+//        var intent = Intent(this, MainActivity::class.java)
+//        startActivity(intent)
+//    }
 }
 
