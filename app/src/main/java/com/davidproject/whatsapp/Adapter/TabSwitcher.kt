61fdc6a145ab33ccc0b532e24e33792536ad379a
David-Lisboa <dlrlisboa@gmail.com>
2021-06-21 21:17:55 -0400

    Tab View finalizado

--- app/src/main/java/com/davidproject/whatsapp/Adapter/TabSwitcher.kt
+++ app/src/main/java/com/davidproject/whatsapp/Adapter/TabSwitcher.kt
@@ -3,51 +3,33 @@ package com.davidproject.whatsapp.Adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.davidproject.whatsapp.Adapter.TabSwitcher
 import com.davidproject.whatsapp.Fragment.ChatFragment
 import com.davidproject.whatsapp.Fragment.ContactFragment
 import com.davidproject.whatsapp.Fragment.GroupChatFragment
 
 class TabSwitcher(fragmentManager: FragmentManager): FragmentPagerAdapter(fragmentManager) {
 
+    override fun getCount(): Int = 3
+
     override fun getItem(position: Int): Fragment {
+        var getFragment: Fragment? = null
 
         when(position) {
-            0 -> {
-                var chatFragment = ChatFragment()
-                return chatFragment
-            }
-            1 -> {
-                var groupChatFragment = GroupChatFragment()
-                return groupChatFragment
-            }
-            2 -> {
-                var contactFragment = ContactFragment()
-                return contactFragment
-            }
-            else -> {
-                return null!!
-            }
+            0 -> getFragment = ChatFragment()
+            1 -> getFragment = GroupChatFragment()
+            2 -> getFragment = ContactFragment()
         }
-    }
-
-    override fun getCount(): Int {
-        return 3
+        return getFragment!!
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
+        var title: String? = null
         when(position) {
-            0->{
-                return "Chat Fragment"
-            }
-            1->{
-                return "Group Chat Fragment"
-            }
-            2->{
-                return "Contact Fragment"
-            }
-            else -> {
-                return null!!
-            }
+            0 -> title = "Chat Fragment"
+            1 -> title = "Group Chat Fragment"
+            2 -> title = "Contact Fragment"
         }
+        return title
     }
 }
--- app/src/main/java/com/davidproject/whatsapp/MainActivity.kt
+++ app/src/main/java/com/davidproject/whatsapp/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.davidproject.whatsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.davidproject.whatsapp.Adapter.TabSwitcher
+import com.davidproject.whatsapp.Fragment.ContactFragment
+import com.davidproject.whatsapp.Fragment.GroupChatFragment
+import com.davidproject.whatsapp.Fragment.ChatFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+   // private lateinit var tabSwitcher: TabSwitcher
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(main_toolbar as Toolbar?)
         supportActionBar?.title = "Whatsapp"
+//
+//        tabSwitcher = TabSwitcher(supportFragmentManager)
+//        main_view_pager.adapter = tabSwitcher
+//        tab_switcher.setupWithViewPager(main_view_pager)
+
+        main_view_pager.adapter = TabSwitcher(supportFragmentManager)
+        tab_switcher.setupWithViewPager(main_view_pager)
+
     }
 }
+
